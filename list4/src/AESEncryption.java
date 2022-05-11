e16a64a153bc9bdb9396542af30c8a6e31971d1b
Bzykowa <karcia956@gmail.com>
2022-05-11 16:13:39 +0200

    Add CBC Encryption

--- list4/src/AESEncryption.java
+++ list4/src/AESEncryption.java
@@ -1,3 +1,104 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.KeyGenerator;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
+
 public class AESEncryption {
-    
+
+    private static final String CBC_ALGORITHM = "AES/CBC/PKCS5Padding";
+
+    /**
+     * Generate AES key
+     * 
+     * @param n Bit length of the key
+     * @return Generated AES SecretKey
+     * @throws NoSuchAlgorithmException
+     */
+    public static SecretKey generateKey(int n) throws NoSuchAlgorithmException {
+        KeyGenerator keyGenerator = KeyGenerator.getInstance("AES");
+        keyGenerator.init(n);
+        return keyGenerator.generateKey();
+    }
+
+    /**
+     * Generate IV for the AES encryption
+     * 
+     * @return IvParameterSpec containing generated IV
+     */
+    public static IvParameterSpec generateIv() {
+        byte[] iv = new byte[16];
+        new SecureRandom().nextBytes(iv);
+        return new IvParameterSpec(iv);
+    }
+
+    /**
+     * Encrypt a file using AES encryption
+     * 
+     * @param algorithm  Mode of operation
+     * @param key        SecretKey for encryption
+     * @param iv         Initialization vector for the encryption
+     * @param inputFile  File to be encrypted
+     * @param outputFile Encrypted file
+     * @throws IOException
+     * @throws NoSuchPaddingException
+     * @throws NoSuchAlgorithmException
+     * @throws InvalidAlgorithmParameterException
+     * @throws InvalidKeyException
+     * @throws BadPaddingException
+     * @throws IllegalBlockSizeException
+     */
+    public static void encryptFileCBC(SecretKey key, IvParameterSpec iv,
+            File inputFile, File outputFile) throws IOException, NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance(CBC_ALGORITHM);
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+        processFiles(cipher, inputStream, outputStream);
+    }
+
+    public static void decryptFileCBC(SecretKey key, IvParameterSpec iv,
+            File inputFile, File outputFile) throws IOException, NoSuchPaddingException,
+            NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException,
+            BadPaddingException, IllegalBlockSizeException {
+
+        Cipher cipher = Cipher.getInstance(CBC_ALGORITHM);
+        cipher.init(Cipher.DECRYPT_MODE, key, iv);
+        FileInputStream inputStream = new FileInputStream(inputFile);
+        FileOutputStream outputStream = new FileOutputStream(outputFile);
+        processFiles(cipher, inputStream, outputStream);
+    }
+
+    private static void processFiles(Cipher cipher, FileInputStream inputStream, FileOutputStream outputStream)
+            throws IOException, IllegalBlockSizeException, BadPaddingException {
+        byte[] buffer = new byte[64];
+        int bytesRead;
+        while ((bytesRead = inputStream.read(buffer)) != -1) {
+            byte[] output = cipher.update(buffer, 0, bytesRead);
+            if (output != null) {
+                outputStream.write(output);
+            }
+        }
+        byte[] outputBytes = cipher.doFinal();
+        if (outputBytes != null) {
+            outputStream.write(outputBytes);
+        }
+        inputStream.close();
+        outputStream.close();
+    }
+
 }
